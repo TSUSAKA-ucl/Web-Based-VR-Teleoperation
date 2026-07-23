e98ecd1647978f2c0d4543f7673ee5969fbe8e2b
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2026-07-23 16:24:50 +0900

    broker,manager,dev-serverのcomposeの起動をMetaworkMQTT(submodule)に移動

--- Robot_Control/SimDocker/up.sh
+++ Robot_Control/SimDocker/up.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+xhost +local:`whoami`
 Here=`pwd -P`
 cd `dirname "$0"` && ThisDir=`pwd -P` && cd -
 if [ $? -ne 0 ]
@@ -23,4 +24,4 @@ fi
 export ROBOT_TYPE="$PKG_NAME"
 export ROBOT_UUID=MA100101000019005100xxx
 export MQTT_BROKER=`ip route get 8.8.8.8 | sed 's/^.*src \+\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\) *.*$/\1/;t;d'`
-docker compose up
+docker compose up "$@"
